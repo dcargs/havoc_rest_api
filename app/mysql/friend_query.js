92b8c3b38454d285dd570cc8fa331740abfd5e02
Devin Cargill <devincargill@Devins-MacBook-Pro.local>
2018-10-30 14:58:45 -0500

    friend/find_friends now functional

--- app/mysql/friend_query.js
+++ app/mysql/friend_query.js
@@ -5,8 +5,9 @@ module.exports = {
     return new Promise(async function(resolve, reject) {
       let query_statement = `SELECT username, first_name, last_name FROM user
                              WHERE user.username NOT IN(SELECT fk_username_1 FROM friend WHERE fk_username_1 = ? OR fk_username_2 = ?)
-                             AND user.username NOT IN(SELECT fk_username_2 FROM friend WHERE fk_username_1 = ? OR fk_username_2 = ?)`;
-      let params = [username, username, username, username];
+                             AND user.username NOT IN(SELECT fk_username_2 FROM friend WHERE fk_username_1 = ? OR fk_username_2 = ?)
+                             AND user.username != ?`;
+      let params = [username, username, username, username, username];
 
       try {
         let result = await query.query(query_statement, params);
